8681707075dd0e5fede79d52adf9068751ba32d9
chmueedzafar <chmueedzafar@gmail.com>
2024-10-28 00:08:43 +0500

    first commit

---
+++ social/tailwind.config.js
@@ -0,0 +1,12 @@
+const withMT = require("@material-tailwind/react/utils/withMT");
+ 
+module.exports = withMT({
+  content: [],
+  theme: {
+    extend:{},
+    fontfamily:{
+      roboto : ["Roboto","sans-serif"]
+    }
+  },
+  plugins: [],
+});
